87d3fad6bdb08ac1cea1abfcb527a202e59d768a
Aleksander Røder <fylling8@gmail.com>
2023-02-20 14:45:44 +0100

    add: sign in page

--- lost-in-translation/src/App.js
+++ lost-in-translation/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import SignInPage from './pages/SignInPage/SignInPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SignInPage />
     </div>
   );
 }
